a99d715219834e52ccd7b8c46b30a4c9cab2b864
Mickey_Cyberkid <63485016+mickey-cyberkid@users.noreply.github.com>
2024-11-12 17:26:56 +0000

    Create App.java

---
+++ src/App.java
@@ -0,0 +1,13 @@
+package radiotherapysuite;
+
+
+/**
+ *
+ * @author Mickey Cyberkid
+ */
+public class App {
+    public static void main(String[] args){
+        RadioTherapySuite.main(args);
+        
+    }
+}
